9e7ab470ca8349c811de9769131e0f9a8a53a583
Ashwin Sreenivas <ashwinsr@stanford.edu>
2018-11-01 19:41:25 -0700

    Abstracted

--- fevd.py
+++ fevd.py
@@ -6,10 +6,28 @@
 # variables.
 #
 # Usage:
-# TODO: explain lag order and default value
-# TODO: explain look ahead and default value
-# TODO: explain return (what is directionality of edges in matrix)
-# TODO: explain usage
+#     from fevd import FEVD
+#     fevd = FEVD('data.csv', ['Column_A', 'Column_B'])
+#
+# This will open the 'data.csv' file and extract the two columns specified and
+# use them as the variables for FEVD. The class also accepts one optional
+# parameter: lag_order.
+#     lag_order: This is the number of values prior to the current timestep
+#                that should be taken into account when computing the VAR. 
+#                Default value is X because.
+#
+#     matrix = fevd.get_matrix(look_ahead)
+#
+#     look_ahead: This is the number of time periods ahead that the forecast
+#                 willl estimate 
+
+
+#FIXME. Default value is 15 because that's what Diebold and Yilmaz use in 2011. This sholud be pertrubed.
+# This will return a 3-tensor of dimensions (look_ahead, K, K). For each of the
+# look_ahead values, it will compute the K x K FEVD matrix. Note, that for each
+# value in the K x K matrix, the value at position (i, j) represent the 
+
+# at that look_ahead value.
 ################################################################################
 
 import pandas as pd
@@ -17,18 +35,16 @@ import numpy as np
 from statsmodels.tsa.api import VAR
 
 class FEVD:
-    def __init__(self, data_file_name, data_columns, lag_order=10, look_ahead=10):
+    def __init__(self, data_file_name, data_columns, lag_order=10):
         # Read raw csv and save columns we want
         data = pd.read_csv(data_file_name)
-        self.data = data[data_columns]
+        data = data[data_columns]
 
-        # Save hyperparameters
-        self.lag_order = lag_order
-        self.look_ahead = look_ahead
 
-    def run(self):
-        model = VAR(self.data.values)
-        results = model.fit(self.lag_order)
-        fevd = results.fevd(self.look_ahead)
+        model = VAR(data.values)
+        self.results = model.fit(lag_order)
+
+    def get_matrix(self, look_ahead=10):
+        fevd = self.results.fevd(look_ahead)
         
-        return np.swapaxes(fevd.decomp, 0, 1)
+        return np.swapaxes(fevd.decomp, 0, 1)[look_ahead-1]
--- test.py
+++ test.py
@@ -4,7 +4,9 @@
 from fevd import FEVD
 
 DATA_FILE = 'test_data.csv'
-DATA_COLUMNS = ['BND.Open', 'GLD.Open', 'IYF.Open']
+#DATA_COLUMNS = ['BND.Open', 'GLD.Open', 'IYF.Open']
+DATA_COLUMNS = ['GLD.Open', 'BND.Open', 'IYF.Open']
+#DATA_COLUMNS = ['IYF.Open', 'BND.Open', 'GLD.Open']
 
 fevd = FEVD(DATA_FILE, DATA_COLUMNS)
-print fevd.run()
+print fevd.get_matrix(10)
